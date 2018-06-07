96867722c86ca4171e13d79cea063326da416189
eprst1234 <oxst@oxst>
2018-06-07 10:42:23 +0300

    fix auth

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
 Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
-    Route::get('/open', ['as' => 'get.open.session', 'uses' => 'ApiController@getOpenSession']);
+    Route::get('/open', ['middleware' => ['auth'], 'as' => 'get.open.session', 'uses' => 'ApiController@getOpenSession']);
     Route::get('/close', ['as' => 'post.close.session', 'uses' => 'ApiController@postCloseSession']);
 });
